e6a33c31cc1c8124a7333efe76def3e86d2d72c7
Scott Shaffer <jsshaff@berkeley.edu>
2018-02-04 13:04:56 -0800

    Some package management...

---
+++ jamr.R
@@ -0,0 +1,31 @@
+
+jamr.load <- function(rootDir, verbose = TRUE) {
+  require(MASS)
+  
+  if (missing(rootDir))
+    rootDir <- jamr.resolveRoot()
+  
+  jamr.loadDir(file.path(rootDir, "plot"),  recursive = TRUE, verbose = verbose)
+
+  options(stringsAsFactors = FALSE)
+  options(width = 120)
+}
+
+jamr.loadDir <- function(dirName, recursive = TRUE, verbose = TRUE) {
+  fileNames <- dir(dirName, pattern = "\\.[rR]$", full.name = TRUE, recursive = recursive)
+
+  for (fileName in fileNames)
+    jamr.loadFile(fileName, verbose = verbose)
+}
+
+jamr.loadFile <- function(fileName, verbose = TRUE) {
+  if (verbose)
+    cat(sprintf("Loading [%s] %s\n", gsub("-", "", as.character(Sys.time())), fileName))
+  
+  source(fileName)
+}
+
+jamr.resolveRoot <- function() {
+  file.path(Sys.getenv("JAMR_HOME"), file.path(Sys.getenv("HOME"), "GitHub", "jamr"))
+}
+
