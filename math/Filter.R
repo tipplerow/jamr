23efd4ebf203208a9ed0a9fb4e6049dbd7c18c38
tipplerow <jsshaff@berkeley.edu>
2019-09-03 18:09:39 -0700

    Adding rank and quantile filters

--- math/Filter.R
+++ math/Filter.R
@@ -1,4 +1,25 @@
 
+Filter.isInteger <- function(x) {
+    floor(x) == ceiling(x)
+}
+
+Filter.quantile <- function(x, n, ties.method = "average") {
+    jam.assert(length(n) == 1 && Filter.isInteger(n) && n >= 2L)
+
+    result <- floor(Filter.rank(x, 1.0, 1.0 + n - 1.0E-12, ties.method))
+    result
+}
+
+Filter.rank <- function(x, lo = 0.0, hi = 1.0, ties.method = "average") {
+    k <- which(is.finite(x))
+
+    result <- rep(NA, length(x))
+    result[k] <- (rank(x[k], ties.method = ties.method) - 1) / (length(k) - 1)
+
+    result <- lo + (hi - lo) * result
+    result
+}
+
 Filter.replaceNA <- function(x, default) {
     x[is.na(x)] <- default
     x
